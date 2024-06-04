e96960ad26e201b8ee15fc942284e97283ec0ac5
Luiz Lima <lima.luizgo@gmail.com>
2024-06-03 23:33:22 -0300

    Initial Commit

---
+++ fiap-cardapio-digital-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fiap-cardapio-digital-ios
+//
+//  Created by Luiz Lima on 03/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fiap-cardapio-digital-ios/fiap_cardapio_digital_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiap_cardapio_digital_iosApp.swift
+//  fiap-cardapio-digital-ios
+//
+//  Created by Luiz Lima on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct fiap_cardapio_digital_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
